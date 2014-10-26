31ebf07098ce7aa32d08d17548a85fe2b1b119ce
Fitzgerald Steele <fitzgeraldsteele@gmail.com>
2014-10-26 01:36:42 +0000

    Changed to php

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<!-- saved from url=(0066)http://twitter.github.com/bootstrap/examples/starter-template.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Eastern Iowa STEM Academy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 40px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merienda:700' rel='stylesheet' type='text/css'>
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/ico/favicon.png">
+ </head>
+
+  <body>
+    
+     <!-- Navbar items -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#myCarousel">Eastern Iowa STEM Academy</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li class="active"><a href="#myCarousel">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#curriculum">Curriculum</a></li>
+              <li><a href="#staff">Staff</a></li>
+              <li><a href="#request">Request Info</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    
+    <div id="myCarousel" class="carousel slide">
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <div class="active item">
+     <!-- Put your photo link in the src attribute -->
+      <img src="https://mindframeeducation.com/wp-content/uploads/2014/10/iStock_000018090704Large.jpg" alt="">
+      <div class="container">
+            <div class="carousel-caption">
+              <h1>21st Century Skills for 21st Century Kids</h1>
+              <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <a class="btn btn-large btn-info pull-right" href="#request">Request More Info</a>
+            </div>
+      </div>
+    </div>
+    <div class="item">
+      <!-- Put your photo link in the src attribute -->
+      <img src="http://placehold.it/1500x550" alt="">
+      <div class="container">
+            <div class="carousel-caption">
+              <h1>Easy to customize</h1>
+              <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <a class="btn btn-large btn-info pull-right" href="#download">Download now</a>
+            </div>
+          </div>
+    </div>
+    <div class="item">
+      <!-- Put your photo link in the src attribute -->
+      <img src="http://placehold.it/1500x550" alt="">
+      <div class="container">
+            <div class="carousel-caption">
+              <h1>Free Download</h1>
+              <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <a class="btn btn-large btn-info pull-right" href="#download">Download now</a>
+            </div>
+          </div>
+    </div>
+  </div>
+  <!-- Carousel nav 
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a> -->
+</div>
+    
+     <!-- Page items -->
+    <div class="container">
+       <!-- About items -->
+      <div id="about">
+        <div id="heading">
+          <h1>K-6 Science, Technology, Engineering, Math Education Experience</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam obcaecati qui velit vitae expedita placeat culpa sunt nobis aut tenetur molestiae maxime magnam beatae dignissimos quam laborum natus consequuntur asperiores. </p>  
+        </div>
+        <div class="divider"></div>
+        <div id="curriculum" class="row">
+          <div class="span4">
+            <!-- Put your photo link in the src attribute -->
+            <img class="img-polaroid" src="media/programming.jpg"  alt="">
+          </div>
+          <div class="span8">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus animi aspernatur rem magnam iure laboriosam sapiente! Rerum quam reiciendis nobis quaerat ut animi ea natus aperiam ipsam tempora dicta libero.</p>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus animi aspernatur rem magnam iure laboriosam sapiente! Rerum quam reiciendis nobis quaerat ut animi ea natus aperiam ipsam tempora dicta libero.</p>
+          </div>
+        </div>
+        <div class="row">
+            <div class="span8">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus animi aspernatur rem magnam iure laboriosam sapiente! Rerum quam reiciendis nobis quaerat ut animi ea natus aperiam ipsam tempora dicta libero.</p>
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus animi aspernatur rem magnam iure laboriosam sapiente! Rerum quam reiciendis nobis quaerat ut animi ea natus aperiam ipsam tempora dicta libero.</p>
+          </div>
+          <div class="span4">
+            <!-- Put your photo link in the src attribute -->
+            <img class="img-polaroid" src="media/scientist.jpg" alt="">
+          </div>
+        </div>
+      </div>
+     
+      <div class="divider"></div>
+      <!-- Screenshot items -->
+      <div id="staff" class="row">
+      <h2>Our Staff</h2>
+      </div>
+      <div class="row">
+        <div class="span6">
+          <img src="media/brooke-circle.png" alt="">
+          <h3>Brookehart Shields, Ph.D.</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos molestiae quae quos fugit voluptates sunt fuga debitis veniam expedita ab ut quidem itaque magnam quo consectetur perferendis odit obcaecati suscipit.</p>
+        </div>
+        <div class="span6">
+          <img src="media/adam-circle.png" alt="">
+          <h3>Adam Specht</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium tenetur voluptate. Debitis ut tempora voluptate repellat. Est dolore provident laudantium veritatis odit necessitatibus quibusdam vitae harum beatae vero labore aliquam?</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="span6">
+          <img src="media/sara-circle.png" alt="">
+          <h3>Sara Steele</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste magni molestiae sed commodi magnam doloribus recusandae dolorum reprehenderit aspernatur ad earum at provident voluptatum expedita laborum fugit praesentium culpa autem!</p>
+        </div>
+        <div class="span6">
+          <img src="media/jerry-circle.png" alt="">
+          <h3>Jerry Steele</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste magni molestiae sed commodi magnam doloribus recusandae dolorum reprehenderit aspernatur ad earum at provident voluptatum expedita laborum fugit praesentium culpa autem!</p>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <!-- Download items -->
+      <div id="request" class="row">
+      <h3>Accepting Applications for August 2015</h3>
+        <p class="text-center">If you would like to learn more about the Eastern Iowa STEM Academy, and be notified when we are accepting applications, enter your email address.</p>
+              <form action="" class="form-inline">
+                <input type="text" class="input-large" placeholder="Email">
+                <button type="submit" class="btn  btn-info">Subscribe</button>
+              </form>
+        
+      </div>
+    </div> <!-- /container -->
+    <!-- Footer items -->
+    <footer>
+        <div class="container">
+          <div class="row">
+            <div class="span6">
+              <h3>About Eastern Iowa STEM Academy</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor deleniti nulla vero fugiat hic impedit laborum ut maxime nobis odio et id ipsam facilis. Molestiae laboriosam dolore eaque cum. Voluptas.</p>
+            </div>
+            <div class="span6">
+              <h3>Get Informed</h3>
+              <ul id="social" class="inline">
+                <li><a class="facebook-icon" href="#"></a></li>
+                <li><a class="twitter-icon" href="#"></a></li>
+                <li><a class="google-icon" href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="span12">
+              <p><small>Copyright 2015, Eastern Iowa STEM Academy</small></p>
+              
+            </div>
+          </div>
+        </div>
+    </footer>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write("<script src='js/jquery-1.8.3.min.js'>\x3C/script>")</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $('a[href^="#"]').click(function(event) {
+            var id = $(this).attr("href");
+            var offset = 60;
+            var target = $(id).offset().top - offset;
+            $('html, body').animate({scrollTop:target}, 800, $.easing.easeInOutExpo());
+            event.preventDefault();
+        });
+      });
+    </script>
+  
+
+</body></html>
